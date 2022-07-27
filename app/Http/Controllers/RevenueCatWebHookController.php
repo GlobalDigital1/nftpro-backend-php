ba554906c1a76f8e14245ff3ede22a896d401e33
ostappp02 <Ostap02pylyp06>
2022-07-27 12:36:43 +0300

    added revenuecat logs

--- app/Http/Controllers/RevenueCatWebHookController.php
+++ app/Http/Controllers/RevenueCatWebHookController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
 
 class RevenueCatWebHookController extends Controller
 {
     public function handle(Request $request)
     {
         $event = $request->get('event');
+        Log::debug($event);
         $user = User::query()->find(Arr::get($event, 'original_app_user_id'));
         $amount = 10;
         if (!$user) {
